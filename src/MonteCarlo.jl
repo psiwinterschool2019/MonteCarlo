51ed0ee37afdc6bce58f4f5d32cd84059f9d3e50
464464mrc <47822470+464464mrc@users.noreply.github.com>
2019-02-20 09:58:03 -0500

    magnetization function

--- src/MonteCarlo.jl
+++ src/MonteCarlo.jl
@@ -3,4 +3,12 @@ using Statistics
 
 module MonteCarlo
 
+function magnetization(c::BitArray{2})::Float64  #This is the magnetization function
+    Lx,Ly=size(c)
+    N=Lx*Ly
+    Nup=count(c)
+    
+    (2Nup-N)/N
+end
+
 end
